41fd8eb13fca5ad75d9edd3adb54af6017fc7378
Vermino <Vermino2004@gmail.com>
2025-11-03 10:29:05 -0800

    fix: Make handleSubmit async to support await

--- src/components/modals/NewCampaignModal.tsx
+++ src/components/modals/NewCampaignModal.tsx
@@ -69,7 +69,7 @@ const NewCampaignModal: React.FC<NewCampaignModalProps> = ({ isOpen, onClose }) 
     }
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!validate()) {
